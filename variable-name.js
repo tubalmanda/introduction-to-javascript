940acf48c575d05eb8e5a38c57beb38bfe443066
Tubal Manda <tubalmanda@gmail.com>
2023-01-26 12:15:08 +0600

    file setup

---
+++ variable-name.js
@@ -0,0 +1,20 @@
+var price = 29;
+
+# Java Script
+
+Programmig Hero
+
+Variable Roles:
+
+1. Varible name can not be any keyword
+2. variable name has to be in one work, No space
+3. Variable name can not have quotation 
+4. variable name can not with a number but can ends with a number 
+5. How to use long name
+can to use dash
+can use under score
+
+var user_name = "bappa raj";
+var userHomeAddress = "andaman island"; // camel case
+var user_home_address = "andaman island"; // snake case - we will use this 
+var UserHomeAddress = "andaman island";  // pascal case
---
+++ variable.js
@@ -0,0 +1,21 @@
+// vary + able = variable 
+// Variable is a storage
+
+// numeric
+var price = 13; 
+var age = 35;
+var temperature = 12;
+var weight = 88;
+var money = 500;
+var sheo = 3;
+
+// string
+var person = "mina das"; 
+var location = "kachijhuli mymensingh"
+var special = 'angel manda';
+
+// Boolean
+var isSingle = false;
+var serious = ture; 
+var isFullMark = true;
+
